fe102c4184c2e75bc34ba0551948b9c4f334e0d6
Apple <bryantconan@yahoo.co.jp>
2020-01-24 09:27:16 +0800

    修改画板

--- GameForm.vb
+++ GameForm.vb
@@ -1,35 +1,31 @@
 ﻿Public Class GameForm
+    Private _gamePadGraphics As Graphics  ' 画板：游戏面板
+    Private _nextBrickGraphics As Graphics  ' 画板：下一个砖块
+
     Private Sub GameForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
     ' 初始化游戏面板的网格线
     Private Sub InitializeGrid()
-        Dim graphics As Graphics = PN_GamePad.CreateGraphics  ' 获取画板
-        Dim pen1 As Pen = New Pen(ConstantData.ColorGridLine, 1)  ' 生成一号画笔(粗一个像素)
-        Dim pen2 As Pen = New Pen(ConstantData.ColorGridLine, 2)  ' 生成一号画笔(粗两个像素)
+        GamePadGraphics = PN_GamePad.CreateGraphics  ' 获取画板
 
         Dim brush As SolidBrush = New SolidBrush(ConstantData.ColorGridLine)  ' 生成画刷
 
         ' 画边框
-        'graphics.DrawLine(pen1, 0, 0, 0, ConstantData.SizeHeight - 1)
-        'graphics.DrawLine(pen1, 0, 0, ConstantData.SizeWidth - 1, 0)
-        'graphics.DrawLine(pen1, ConstantData.SizeWidth - 1, 0, ConstantData.SizeWidth - 1, ConstantData.SizeHeight - 1)
-        'graphics.DrawLine(pen1, 0, ConstantData.SizeHeight - 1, ConstantData.SizeWidth - 1, ConstantData.SizeHeight - 1)
-
-        graphics.FillRectangle(brush, 0, 0, 1, ConstantData.SizeHeight)
-        graphics.FillRectangle(brush, 0, 0, ConstantData.SizeWidth, 1)
-        graphics.FillRectangle(brush, ConstantData.SizeWidth - 1, 0, 1, ConstantData.SizeHeight)
-        graphics.FillRectangle(brush, 0, ConstantData.SizeHeight - 1, ConstantData.SizeWidth, 1)
+        GamePadGraphics.FillRectangle(brush, 0, 0, 1, ConstantData.SizeHeight)
+        GamePadGraphics.FillRectangle(brush, 0, 0, ConstantData.SizeWidth, 1)
+        GamePadGraphics.FillRectangle(brush, ConstantData.SizeWidth - 1, 0, 1, ConstantData.SizeHeight)
+        GamePadGraphics.FillRectangle(brush, 0, ConstantData.SizeHeight - 1, ConstantData.SizeWidth, 1)
 
         ' 画网格线
         ' 画水平网格线
         For row As Integer = 1 To ConstantData.CountVertical
-            graphics.FillRectangle(brush, 0, ConstantData.Delta * row - 1, ConstantData.SizeWidth, 2)
+            GamePadGraphics.FillRectangle(brush, 0, ConstantData.Delta * row - 1, ConstantData.SizeWidth, 2)
         Next
         ' 画垂直网格线
         For collumn As Integer = 1 To ConstantData.CountHorizon
-            graphics.FillRectangle(brush, ConstantData.Delta * collumn - 1, 0, 2, ConstantData.SizeHeight)
+            GamePadGraphics.FillRectangle(brush, ConstantData.Delta * collumn - 1, 0, 2, ConstantData.SizeHeight)
         Next
 
     End Sub
@@ -38,4 +34,23 @@
     Private Sub MI_Start_Click(sender As Object, e As EventArgs) Handles MI_Start.Click
         InitializeGrid()
     End Sub
+
+    Public Property GamePadGraphics As Graphics
+        Get
+            Return _gamePadGraphics
+        End Get
+        Set(value As Graphics)
+            _gamePadGraphics = value
+        End Set
+    End Property
+
+    Public Property NextBrickGraphics As Graphics
+        Get
+            Return _nextBrickGraphics
+        End Get
+        Set(value As Graphics)
+            _nextBrickGraphics = value
+        End Set
+    End Property
+
 End Class
